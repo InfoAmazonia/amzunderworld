e07b4fdedd4b7b61086b427e4ac9a1b35a2dccfa
stroligo <gabrielstroligo@gmail.com>
2023-07-20 20:32:31 +0200

    Front Feito

--- teste.php
+++ teste.php
@@ -1,251 +1,35 @@
 <?php $title = 'Welcome to the Amazon Underworld'; ?>
 <?php include './blocks/header.php'; ?>
-
 <!-- wp:code -->
+<?php include './build/svg.php'; ?>
+
+
 <link rel="stylesheet" type="text/css" href="https://taprontodev.github.io/amzunderworld/build/style.css" />
 <div class="amz" id="story0">
+
 <!-- /wp:code -->
 
-<!-- wp:cover {"url":"https://infoamazonia.org/wp-content/uploads/2023/07/header_story0.png","id":159453,"dimRatio":0,"align":"full","className":"header-story"} -->
-<div class="wp-block-cover alignfull header-story"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-159453" alt="" src="https://infoamazonia.org/wp-content/uploads/2023/07/header_story0.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Escreva o título...","fontSize":"large"} -->
+<!-- wp:cover {"url":"https://infoamazonia.org/wp-content/uploads/2023/07/header_story1.png","id":159453,"dimRatio":0,"align":"full","className":"header-story"} -->
+<div class="wp-block-cover alignfull header-story"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-159453" alt="" src="https://infoamazonia.org/wp-content/uploads/2023/07/header_story1.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Escreva o título...","fontSize":"large"} -->
 <p class="has-text-align-center has-large-font-size"></p>
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:cover -->
 
-<!-- wp:paragraph {"className":"big"} -->
-<p class="big">The Amazon, known as the largest rainforest in the world, also functions as a source and transit point for illegally extracted jungle resources and narcotics, linking nine countries together.</p>
-<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"className":"big"} -->
-<p class="big">As criminal economies expand in size, the levels of violence and deforestation worsen, and the Amazon and its communities face an existential threat.</p>
-<!-- /wp:paragraph -->
 
-<!-- wp:paragraph -->
-<p>Beneath the lush canopy of the world's most biodiverse ecosystem lies a darker reality, hidden behind the natural splendor. A labyrinth of thousands of rivers allows criminal groups to move freely while the dense forest cover enables them to evade detection.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>The remoteness and lack of infrastructure cause a profound disconnect between Amazon communities and capital cities. Historically neglected by their governments, entire regions and populations are left isolated and excluded.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Porous and difficult to control, borderlands are marked by an obscure convergence of guerrilla movements, criminal startups and multinational organized crime.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Armed groups, drug traffickers and illegal miners encroach on or invade protected areas and Indigenous territories. In some places, park guards have been forced to abandon their posts.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Some Indigenous communities rent their land for drug crops or mining. Those who resist risk assassination. Despite efforts to protect the Amazon and its indigenous and traditional peoples, criminal groups have seized control of remote regions.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>This is the Amazon Underworld, where cycles of crime &amp; destruction are fueled by a multibillion-dollar illegal trade in drugs, gold and arms.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:cover {"url":"https://infoamazonia.org/wp-content/uploads/2023/07/banner-green.png","id":159441,"dimRatio":0,"align":"full"} -->
-<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-159441" alt="" src="https://infoamazonia.org/wp-content/uploads/2023/07/banner-green.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"textColor":"white"} -->
-<h1 class="wp-block-heading has-text-align-center has-white-color has-text-color" id="h-story-map-laura">STORY MAP LAURA</h1>
-<!-- /wp:heading --></div></div>
-<!-- /wp:cover -->
-
-<!-- wp:image {"id":159450,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/faca.png" alt="" class="wp-image-159450"/></figure>
+<!-- wp:image {"id":159859,"width":662,"height":405,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized border-full"><img src="./src/img/story1-img2.png" alt="" class="wp-image-159859" width="662" height="405"/></figure>
 <!-- /wp:image -->
 
-<!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading" id="h-criminal-armed-groups">CRIMINAL &amp; ARMED GROUPS</h1>
-<!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>Certain criminal groups, such as Colombian guerrilla outfits and the Manaus-based crime organization Familia do Norte, have a longstanding history in the Amazon. However, there has been a notable surge in the presence of other criminal groups in the region in recent years.</p>
-<!-- /wp:paragraph -->
 
-<!-- wp:code -->
-<!-- Componente Banner Mapa -->
-<section class="full borda01 mapa" style="background-image: url(https://infoamazonia.org/wp-content/uploads/2023/07/colombia_venezuela.png);">
-<div class="content">
-<div class="twocols">
-<div>
-<div class="tags">
-<button class="tag ativo">Exército de Libertação Nacional</button>
-<button class="tag">farc dissidents</button>
-<button class="tag">primeiro comando da capital</button>
-<button class="tag">comando vermelho</button>
-<button class="tag">sindicatos</button>
-</div>
-<div class="title">
-<span>Exército de Libertação Nacional, ELN</span>
-<img src="https://infoamazonia.org/wp-content/uploads/2023/07/eln.png">
-</div>
-<div class="linha">
-<span class="item">Main presence:</span>
-<span class="value">Colombia &amp; Venezuela</span>
-</div>
-<div class="linha" style="flex-direction: row;">
-<div class="coll" style="margin-right:6rem;">
-<span class="item">Est. members:</span>
-<span class="value">XXX.XXX</span>
-</div>
-<div class="coll">
-<span class="item">Founded:</span>
-<span class="value">1964</span>
-</div>
-<p></p></div>
-<div class="linha">
-<span class="item">Political/ideological stance:</span>
-<span class="value">Marxist guerrilla group</span>
-</div>
-<div class="linha">
-<span class="item">Main economies:</span>
-<span class="value">Illegal gold mining and taxing the drug trade</span>
-</div>
-</div>
-</div>
-</div>
-</section>
-<!-- /wp:code -->
-
-<!-- wp:paragraph -->
-<p>Country borders closed down during the global pandemic, but criminal enterprises know no international frontiers and have increased their control while governments limited their actions in the Amazon.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Remote border regions have transformed into havens for criminal networks, where they interact, engage in illicit trade, fuel conflicts, cross borders to evade law enforcement, and expand their illegal economies.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>One example is the triple border between Colombia and Peru, the two largest producers of cocaine globally, and Brazil, which serves as both a significant consumer market and a crucial transit area. In this region, the Amazon River acts as the starting point for cocaine shipments that are transported to port cities along Brazil's Atlantic Coast. From there, the drugs are distributed to markets in Europe, Africa, and the Middle East.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:cover {"url":"https://infoamazonia.org/wp-content/uploads/2023/07/banner-green.png","id":159441,"dimRatio":0,"align":"full"} -->
-<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-159441" alt="" src="https://infoamazonia.org/wp-content/uploads/2023/07/banner-green.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1,"textColor":"white"} -->
-<h1 class="wp-block-heading has-text-align-center has-white-color has-text-color" id="h-armed-groups-map-laura">ARMED GROUPS MAP LAURA</h1>
-<!-- /wp:heading --></div></div>
-<!-- /wp:cover -->
-
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">CONHEÇA A METODOLOGIA</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons -->
-
-<!-- wp:spacer {"height":"63px"} -->
-<div style="height:63px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:paragraph -->
-<p>One Colombian crime boss says that country’s Amazon borderlands are a hotbed for organized crime. “This is a warehouse, a Persian market. You can get everything from a .38-caliber gun to a surface-to-air missile here in the Amazon.”</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>As the future of the Amazon is at stake, criminal groups launder their profits through land speculation and seemingly legal activities like cattle ranching. The process destroys&nbsp; Amazonian forests and ensnares Indigenous and other traditional communities in spiraling cycles of violence.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>In our project ‘Amazon Underworld’ we dive into crime dynamics in the Amazon and cover the social, cultural and environmental impacts of illicit economies that are changing the face of the jungle forever.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Through a cross-border approach and the application of qualitative and quantitative methods, we have worked with a collaborative network of journalists to map armed groups, criminal organizations and illicit economies of the Amazon borderlands in Bolivia, Brazil, Colombia, Ecuador, Peru and Venezuela.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"className":"big"} -->
-<p class="big">In 15 months of research in the field and behind our laptops, using satellite images and building new databases, a collective effort of 24 journalists from 9 countries narrates the Amazon Underworld from within through eight in-depth stories.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph -->
-<p>Amazon Underworld is a joint investigation of InfoAmazonia (Brazil), Armando.Info (Venezuela) and La Liga Contra el Silencio (Colombia). The work is carried out in collaboration with the Pulitzer Center’s Rainforest Investigations Network and financed by the Open Society Foundation and the U.K. Foreign, Commonwealth &amp; Development Office.</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:spacer {"height":"18px"} -->
-<div style="height:18px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">VOLTAR AO TOPO</a></div>
-<!-- /wp:button -->
-
-<!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">PAGINA INICIAL</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons -->
-
-<!-- wp:spacer {"height":"18px"} -->
-<div style="height:18px" aria-hidden="true" class="wp-block-spacer"></div>
-<!-- /wp:spacer -->
-
-<!-- wp:image {"id":159450,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/megafone.png" alt="" class="wp-image-159450"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading" id="explore the stories">EXPLORE THE STORIES</h1>
-<!-- /wp:heading -->
-
-<!-- wp:code -->
-<p>    <!-- Componente Banner Cards --></p>
-<section class="full cards">
-<div class="card">
-<div class="image"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/card1.png"></div>
-<div class="infos">
-<div class="local">country + country</div>
-<div class="title">Gold spurs crime &amp; corruption on Brazil-Colombia border</div>
-<p>                <button class="btn">read more <span class="arrow"></span></button>
-            </p></div>
-<p></p></div>
-<div class="card">
-<div class="image"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/card2.png"></div>
-<div class="infos">
-<div class="local">country + country</div>
-<div class="title">Gold spurs crime &amp; corruption on Brazil-Colombia border</div>
-<p>                <button class="btn">read more <span class="arrow"></span></button>
-            </p></div>
-<p></p></div>
-<div class="card">
-<div class="image"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/card3.png"></div>
-<div class="infos">
-<div class="local">country + country</div>
-<div class="title">Gold spurs crime &amp; corruption on Brazil-Colombia border</div>
-<p>                <button class="btn">read more <span class="arrow"></span></button>
-            </p></div>
-<p></p></div>
-<div class="card">
-<div class="image"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/card1.png"></div>
-<div class="infos">
-<div class="local">country + country</div>
-<div class="title">Gold spurs crime &amp; corruption on Brazil-Colombia border</div>
-<p>                <button class="btn">read more <span class="arrow"></span></button>
-            </p></div>
-<p></p></div>
-</section>
-<!-- /wp:code -->
-
-<!-- wp:cover {"id":159441,"dimRatio":0,"isDark":false,"className":"bg-white"} -->
-<div class="wp-block-cover is-light bg-white"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:image {"id":159450,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/radio.png" alt="Icone Rádio" class="wp-image-159450"/></figure>
-<!-- /wp:image -->
-
-<!-- wp:heading {"level":1} -->
-<h1 class="wp-block-heading" id="explore the stories">TODAS AS HISTÓRIAS DO PROJETO</h1>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Conheça</a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div></div>
-<!-- /wp:cover -->
 
 <!-- wp:code -->
 </div >
 <!-- /wp:code -->
+
+<p><!-- /wp:code --><br>
+<!-- /wp:code --></p>
 
 <!-- Footer -->
 <?php include './blocks/footer.php'; ?>
