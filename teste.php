acfa591a4c75c43b7fc38c9f10e64474cbf79ee2
stroligo <gabrielstroligo@gmail.com>
2023-07-21 19:47:44 +0200

    Front Feito

--- teste.php
+++ teste.php
@@ -21,3 +21,21 @@
 
 <!-- Footer -->
 <?php include './blocks/footer.php'; ?>
+
+
+
+<!-- wp:image {"align":"left","id":160021,"width":358,"height":269,"sizeSlug":"full","linkDestination":"none","className":"border-full"} -->
+<figure class="wp-block-image alignleft size-full is-resized border-full"><img src="https://infoamazonia.org/wp-content/uploads/2023/07/story1-img12.png" alt="" class="wp-image-160021" width="358" height="269"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"level":1,"className":"tbege"} -->
+<h1 class="wp-block-heading tbege" id="h-at-us-54-a-gram">AT US$ 54 A GRAM,</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"big"} -->
+<p class="big">the price in Japurá, a smuggler or a dredge owner could be carrying the value of a middle-class apartment in Manaus in his pocket.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>A man in a shop in Manaus displays a one-kilogram bar of gold | Rodrigo Pedroso</p>
+<!-- /wp:paragraph -->
