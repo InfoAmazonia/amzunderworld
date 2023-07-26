b4b0a90bde00ac743c65abfebddbfb1258d6274d
stroligo <gabrielstroligo@gmail.com>
2023-07-26 16:16:38 +0200

    Front Feito

---
+++ testes.php
@@ -0,0 +1,12 @@
+<?php $title = 'Story 1'; ?>
+<?php include './blocks/header.php'; ?>
+
+<style>
+<?php include './src/less/style.css'; ?>
+</style>
+
+
+
+
+<!-- Footer -->
+<?php include './blocks/footer.php'; ?>
