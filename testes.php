cc4b3480a56cffea333fee5a21a1ebc1f09628a7
stroligo <gabrielstroligo@gmail.com>
2023-07-30 15:43:49 +0200

    Front Feito

--- testes.php
+++ testes.php
@@ -21,21 +21,47 @@
                 <p class="caption">Video: Google Earth</p>
             </div>
         </div>
+
         <div class="scrollytelling-inner">
             <div class="box">
-                <p>In Brazil’s Amazonas state, near the border with Colombia, illegal mining is fueled by rising gold prices and surrounded by violence and corruption.</p>
-            </div>        
+                <p class="big">Dangerous and lawless,</p>
+                <p>this area has become a magnet for fortune seekers, modern-day pirates, rogue law-enforcement officers & Colombian guerrillas.</p>
+                <p class="caption">Video: Bram Ebus</p>
+            </div>
         </div>
+
         <div class="scrollytelling-inner">
             <div class="box">
-                <p>In Brazil’s Amazonas state, near the border with Colombia, illegal mining is fueled by rising gold prices and surrounded by violence and corruption.</p>
-            </div>        
+                <p>Some illegal miners, known as garimpeiros, worked on smaller dredges in the area as early as the 1980s.</p>
+                <p class="big">But hundreds of industrial-size mining barges arrived in the municipality of Japurá, in the state of Amazonas, after former Brazilian President Jair Bolsonaro took office in 2019 with a pledge to legalize informal gold mining.</p>
+                <p class="caption">Photo: Andrés Cardona</p>
+            </div>
         </div>
+
         <div class="scrollytelling-inner">
             <div class="box">
-                <p>In Brazil’s Amazonas state, near the border with Colombia, illegal mining is fueled by rising gold prices and surrounded by violence and corruption.</p>
-            </div>        
+                <p class="big">Besides scooping up so much sediment that they change the course of the river, the miners dump hundreds of kilograms of mercury into the environment.</p>
+                <p>The dredges also encroach on protected areas, including one inhabited by a nomadic Indigenous tribe that shuns contact with outsiders.</p>
+                <p class="caption">Photo: Andrés Cardona</p>
+            </div>
         </div>
+
+        <div class="scrollytelling-inner">
+            <div class="box">
+                <p>Both Brazil’s and Colombia’s new presidents, Lula da Silva and Gustavo Petro, have pledged</p>
+                <p class="big">to rein in the illegal mining that churns out hundreds of millions of dollars’ worth of gold, financing armed groups on both sides of the border.</p>
+                <p class="caption">Photo: Alex Rufino</p>
+            </div>
+        </div>
+
+        <div class="scrollytelling-inner">
+            <div class="box">
+                <p>Nevertheless, the illegal gold mining that has swept through the Amazon, combined with drug trafficking,</p>
+                <p class="big">Besides scooping up so much sediment that they change the course of the river, the miners dump hundreds of kilograms of mercury into the environment.</p>
+                <p class="caption">Photo: Alex Rufino</p>
+            </div>
+        </div>
+
     </div>
 </div>
 <!-- /wp:cover -->
