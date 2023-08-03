e4471cd2c08dbef0ed77b516c8a63bdca40dd404
Erico <erico@tapronto.com.br>
2023-08-03 16:14:07 -0300

    Dados

--- story7.php
+++ story7.php
@@ -17,16 +17,16 @@
 
 <script type="text/javascript">
     const carcerarios = [
-        {ano:2013, rr:300,br:350, rrdif:"-1%", brdif:"+5%"},
-        {ano:2014, rr:350,br:450, rrdif:"-1%", brdif:"+5%"},
-        {ano:2015, rr:370,br:350, rrdif:"-1%", brdif:"+5%"},
-        {ano:2016, rr:320,br:550, rrdif:"-1%", brdif:"+5%"},
-        {ano:2017, rr:300,br:650, rrdif:"-1%", brdif:"+5%"},
-        {ano:2018, rr:250,br:450, rrdif:"-1%", brdif:"+5%"},
-        {ano:2019, rr:200,br:350, rrdif:"-1%", brdif:"+5%"},
-        {ano:2020, rr:350,br:250, rrdif:"-1%", brdif:"+5%"},
-        {ano:2021, rr:380,br:550, rrdif:"-1%", brdif:"+5%"},
-        {ano:2022, rr:400,br:350, rrdif:"-1%", brdif:"+5%"}
+        {ano:2013, rr:338.63,br:300.96, rrdif:"", brdif:""},        
+        {ano:2014, rr:321.7,br:306.2, rrdif:"-5,00%", brdif:"+1,74%"},
+        {ano:2015, rr:441.4,br:341.7, rrdif:"+37,21%", brdif:"+11,59%"},
+        {ano:2016, rr:315.6,br:350.4, rrdif:"-28,50%", brdif:"+2,55%"},
+        {ano:2017, rr:520.63,br:348.03, rrdif:"+64,97% ", brdif:"-0,68%"},
+        {ano:2018, rr:526.33,br:356.95, rrdif:"+1,09%", brdif:"+2,56%"},
+        {ano:2019, rr:612.6,br:359.4, rrdif:"+16,39%", brdif:"+0,69%"},
+        {ano:2020, rr:655.91,br:383.32, rrdif:"+7,07%", brdif:"+6,66%"},
+        {ano:2021, rr:854.43,br:390.58, rrdif:"+30,27%", brdif:"+1,89%"},
+        {ano:2022, rr:702.76,br:390.17, rrdif:"-17,75%", brdif:"-0,10%"} 
     ];
 </script>
 <div id="graficocarcerarios"></div>
