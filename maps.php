0a49f9a0f0aab5df53c2612ee3955182d0b7c082
Erico <erico@tapronto.com.br>
2023-08-03 16:52:08 -0300

    mapas

---
+++ maps.php
@@ -0,0 +1,48 @@
+<?php $title = 'Story 1'; ?>
+<?php include './blocks/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="./build/style.css" />
+
+<!-- wp:code -->
+<div class="amz" id="story1">
+<!-- /wp:code -->
+
+
+
+
+<!-- wp:jeo/embedded-storymap {"storyID":161071} -->
+{"attributes":{"storyID":161071},"innerBlocks":[]}
+<!-- /wp:jeo/embedded-storymap -->
+
+
+
+
+
+
+
+
+<!-- wp:html -->
+<section class="full mapameio">
+<!-- /wp:html -->
+
+<!-- wp:jeo/map {"map_id":164051} -->
+<div class="jeomap wp-block-jeo-map map_id_164051"></div>
+<!-- /wp:jeo/map -->
+
+<!-- wp:html -->
+</section>
+<!-- /wp:html -->
+
+
+
+
+
+
+
+
+
+<!-- wp:code -->
+</div>
+<!-- /wp:code -->
+
+<!-- Footer -->
+<?php include './blocks/footer.php'; ?>
